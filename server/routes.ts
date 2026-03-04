655c10ed9f478bc9b93d6936ad8f7a7582860add
deepanshu-80 <deepanshuruhela18@gmail.com>
2026-03-05 00:22:26 +0530

    fix static import

--- server/routes.ts
+++ server/routes.ts
@@ -1,6 +1,6 @@
 import type { Express } from "express";
 import { createServer, type Server } from "http";
-import { storage } from "./storage";
+import { storage } from "./storage.ts";
 import { api } from "@shared/routes";
 import { z } from "zod";
 import session from "express-session";
